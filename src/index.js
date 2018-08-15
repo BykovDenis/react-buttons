dbe5798cbfc48ee27d03d36156fdb5cfda1f8f1a
Denis <stivenrain@gmail.com>
2018-08-16 01:08:55 +0300

    initial

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React, { Fragment, Component } from 'react';
+import './index.css';
+
+class ButtonBlue extends Component {
+  render() {
+    return (
+      <Fragment>
+        <button className="button-blue">
+          Blue Button
+        </button>
+      </Fragment>
+    );
+  }
+}
+
+export default ButtonBlue;
